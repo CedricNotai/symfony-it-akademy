f1c404375a58fade6326e0778f88fe95ab6c2f32
CedricNotai <cedric.cam@hotmail.fr>
2022-05-16 18:01:34 +0200

    Initial stable architecture

---
+++ app/src/Controller/QuoteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\QuoteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuoteController extends AbstractController {
+
+    #[Route('/', name: 'index')]
+    
+    // @Route('', name:'index')
+    // @param QuoteRepository $quoteRepository
+    // @return Response
+
+    public function index(
+        QuoteRepository $quoteRepository
+    )
+    : Response {
+
+        return $this->render(
+            'quote/index.html.twig',
+            [
+                'quotes' => $quoteRepository->findAll(),
+            ]
+        );
+    }
+}
